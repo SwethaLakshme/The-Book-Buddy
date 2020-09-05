7cad047e2800940f47ad47b2705ccada88e0109a
Thoshinny-cyber <sec19cs051@sairamtap.edu.in>
2020-09-05 16:38:50 +0530

    Added new routes

---
+++ class8cbsesubs.py
@@ -0,0 +1,2 @@
+from app import app
+from flask import Flask, render_template, url_for
--- routes.py
+++ routes.py
@@ -13,7 +13,26 @@ def cbsetextbook():
 def stateboardtextbook():
     return render_template('stateboardtextbook.html')
 
+# CBSE Standards
 
-@app.route('/eighthcbse')
-def eighthcbse():
-    return render_template('cbse/8th/8thcbse.html')
+from cbsestandards import *
+
+# StateBoard Standards
+
+from stateboardstandards import *
+
+# CBSE Subjects 
+
+from class8cbsesubs import *
+from class9cbsesubs import *
+from class10cbsesubs import *
+from class11cbsesubs import *
+from class12cbsesubs import *
+
+# Stateboard Subjects 
+
+from class8stateboardsubs import *
+from class9stateboardsubs import *
+from class10stateboardsubs import *
+from class11stateboardsubs import *
+from class12stateboardsubs import *
