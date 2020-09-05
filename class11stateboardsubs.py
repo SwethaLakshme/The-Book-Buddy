a33e8d9ec5af96519004f82859ad581c39e7e13d
Thoshinny-cyber <sec19cs051@sairamtap.edu.in>
2020-09-06 03:14:57 +0530

    Updated all stateboard books

--- class11stateboardsubs.py
+++ class11stateboardsubs.py
@@ -1,9 +1,9 @@
 from app import app
 from flask import Flask, render_template, url_for
 
-@app.route('/class11state_accounts')
-def class11state_accounts():
-    return render_template('state/11th/accounts.html')
+@app.route('/class11state_accountancy')
+def class11state_accountancy():
+    return render_template('state/11th/accountancy.html')
 
 @app.route('/class11state_botany')
 def class11state_botany():
@@ -13,9 +13,9 @@ def class11state_botany():
 def class11state_zoology():
     return render_template('state/11th/zoology.html')
 
-@app.route('/class11state_business_maths')
-def class11state_business_maths():
-    return render_template('state/11th/business_maths.html')
+@app.route('/class11state_businessmaths')
+def class11state_businessmaths():
+    return render_template('state/11th/businessmaths.html')
 
 @app.route('/class11state_chemistry')
 def class11state_chemistry():
@@ -25,29 +25,9 @@ def class11state_chemistry():
 def class11state_commerce():
     return render_template('state/11th/commerce.html')
 
-@app.route('/class11state_computer_science')
-def class11state_computer_science():
-    return render_template('state/11th/computer_science.html')
-
-@app.route('/class11state_economics')
-def class11state_economics():
-    return render_template('state/11th/economics.html')
-
-@app.route('/class11state_english')
-def class11state_english():
-    return render_template('state/11th/english.html')
-
-@app.route('/class11state_french')
-def class11state_french():
-    return render_template('state/11th/french.html')
-
-@app.route('/class11state_maths')
-def class11state_maths():
-    return render_template('state/11th/maths.html')
-
-@app.route('/class11state_microbiology')
-def class11state_microbiology():
-    return render_template('state/11th/microbiology.html')
+@app.route('/class11state_computerscience')
+def class11state_computerscience():
+    return render_template('state/11th/computerscience.html')
 
 @app.route('/class11state_physics')
 def class11state_physics():
@@ -56,3 +36,7 @@ def class11state_physics():
 @app.route('/class11state_tamil')
 def class11state_tamil():
     return render_template('state/11th/tamil.html')
+
+@app.route('/class11state_english')
+def class11state_english():
+    return render_template('state/11th/english.html')
--- class12stateboardsubs.py
+++ class12stateboardsubs.py
@@ -1,9 +1,9 @@
 from app import app
 from flask import Flask, render_template, url_for
 
-@app.route('/class12state_accounts')
-def class12state_accounts():
-    return render_template('state/12th/accounts.html')
+@app.route('/class12state_economics')
+def class12state_economics():
+    return render_template('state/12th/economics.html')
 
 @app.route('/class12state_botany')
 def class12state_botany():
@@ -13,9 +13,9 @@ def class12state_botany():
 def class12state_zoology():
     return render_template('state/12th/zoology.html')
 
-@app.route('/class12state_business_maths')
-def class12state_business_maths():
-    return render_template('state/12th/business_maths.html')
+@app.route('/class12state_businessmaths')
+def class12state_businessmaths():
+    return render_template('state/12th/businessmaths.html')
 
 @app.route('/class12state_chemistry')
 def class12state_chemistry():
@@ -25,25 +25,9 @@ def class12state_chemistry():
 def class12state_commerce():
     return render_template('state/12th/commerce.html')
 
-@app.route('/class12state_computer_science')
-def class12state_computer_science():
-    return render_template('state/12th/computer_science.html')
-
-@app.route('/class12state_economics')
-def class12state_economics():
-    return render_template('state/12th/economics.html')
-
-@app.route('/class12state_english')
-def class12state_english():
-    return render_template('state/12th/english.html')
-
-@app.route('/class12state_french')
-def class12state_french():
-    return render_template('state/12th/french.html')
-
-@app.route('/class12state_maths')
-def class12state_maths():
-    return render_template('state/12th/maths.html')
+@app.route('/class12state_computerscience')
+def class12state_computerscience():
+    return render_template('state/12th/computerscience.html')
 
 @app.route('/class12state_physics')
 def class12state_physics():
@@ -52,3 +36,7 @@ def class12state_physics():
 @app.route('/class12state_tamil')
 def class12state_tamil():
     return render_template('state/12th/tamil.html')
+
+@app.route('/class12state_english')
+def class12state_english():
+    return render_template('state/12th/english.html')
